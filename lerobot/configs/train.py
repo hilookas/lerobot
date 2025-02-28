0bc75b26cdb501759bc9f6c9976cd2fef28ed0ad
lookas <i@18kas.com>
2025-02-28 11:07:08 +0000

    Add steps

--- lerobot/configs/train.py
+++ lerobot/configs/train.py
@@ -45,7 +45,7 @@ class TrainPipelineConfig(HubMixin):
     # Number of workers for the dataloader.
     num_workers: int = 4
     batch_size: int = 8
-    steps: int = 100_000
+    steps: int = 1_000_000
     eval_freq: int = 20_000
     log_freq: int = 200
     save_checkpoint: bool = True
